504f8ba9726eee2c11f000d7a0a5d7c7ea5468cc
tamilanban <anbantamizh@gmail.com>
2020-06-19 16:57:53 +1000

    adds unit test

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,39 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+
+const cats = {
+	'Coding Cat': 'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
+	'Compiling Cat': 'https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif'
+  };
+  
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
+
+	let catcommand =     vscode.commands.registerCommand('catCoding.start', () => {
+		const panel = vscode.window.createWebviewPanel(
+		  'catCoding',
+		  'Cat Coding',
+		  vscode.ViewColumn.One,
+		  {}
+		);
+  
+		let iteration = 0;
+		const updateWebview = () => {
+		  const cat = iteration++ % 2 ? 'Compiling Cat' : 'Coding Cat';
+		  panel.title = cat;
+		  panel.webview.html = getWebviewContent(cat);
+		};
+  
+		// Set initial content
+		updateWebview();
+  
+		// And schedule updates to the content every second
+		setInterval(updateWebview, 1000);
+	  });
+	context.subscriptions.push(catcommand);
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "atomist-test" is now active!');
@@ -23,5 +52,21 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
+
+
+
+export  function getWebviewContent(cat: keyof typeof cats) {
+	return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>Cat Coding</title>
+  </head>
+  <body>
+	  <img src="${cats[cat]}" width="300" />
+  </body>
+  </html>`;
+  }; 
 // this method is called when your extension is deactivated
 export function deactivate() {}
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,9 +1,11 @@
 import * as assert from 'assert';
+import * as myExtension from './../../extension';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
+
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
@@ -11,5 +13,24 @@ suite('Extension Test Suite', () => {
 	test('Sample test', () => {
 		assert.equal(-1, [1, 2, 3].indexOf(5));
 		assert.equal(-1, [1, 2, 3].indexOf(0));
+	});
+
+	test('test get webview content', () => {
+		let iteration = 0;
+		const cat = iteration++ % 2 ? 'Compiling Cat' : 'Coding Cat';
+		 var result = myExtension.getWebviewContent(cat);
+         var expectedResult = `<!DOCTYPE html>
+		 <html lang="en">
+		 <head>
+			 <meta charset="UTF-8">
+			 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+			 <title>Cat Coding</title>
+		 </head>
+		 <body>
+			 <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+		 </body>
+		 </html>`;
+
+		assert.equal(result,expectedResult,"template mismatch");
 	});
 });
